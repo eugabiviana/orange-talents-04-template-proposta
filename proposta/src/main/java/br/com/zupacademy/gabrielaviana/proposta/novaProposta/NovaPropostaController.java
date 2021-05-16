4c7c2895129a278fa4dd706ac2aae8b7c3e32373
eugabiviana <gabi.viana88@gmail.com>
2021-05-16 14:20:33 -0300

    Validação para que não haja mais de uma proposta com o mesmo solicitante

--- proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/novaProposta/NovaPropostaController.java
+++ proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/novaProposta/NovaPropostaController.java
@@ -22,9 +22,15 @@ public class NovaPropostaController {
     @PostMapping
     public ResponseEntity<NovaPropostaDto> cadastrar(@RequestBody @Valid NovaPropostaForm form,
                                                      UriComponentsBuilder uriComponentsBuilder){
+
+        //usar o exist, retorna apenas o id do objeto enquanto o findBy retorna o objeto completo
+        //método para não criar duas propostas com documentos iguais
+        if(propostaRepository.existsByDocumento(form.getDocumento())){
+            return ResponseEntity.unprocessableEntity().build();
+        }
         Proposta novaProposta = form.paraProposta();
 
-        //salvar a proposta
+        //salvar a proposta mostrando o endereço de onde os dados foram salvos
         propostaRepository.save(novaProposta);
         URI location = uriComponentsBuilder.path("/propostas/{id}")
                                            .buildAndExpand(novaProposta.getId())
--- proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/novaProposta/PropostaRepository.java
+++ proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/novaProposta/PropostaRepository.java
@@ -3,4 +3,5 @@ package br.com.zupacademy.gabrielaviana.proposta.novaProposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+    boolean existsByDocumento(String documento);
 }
