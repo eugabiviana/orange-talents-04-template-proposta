5641d53e2da03feecb0383807f82f4487cb24232
eugabiviana <gabi.viana88@gmail.com>
2021-05-23 15:06:10 -0300

    Dockerfile - rodando a aplicacao

--- proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/cartoes/CartaoClientFeign.java
+++ proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/cartoes/CartaoClientFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="solicitacao-cartao", url= "http://localhost:8888")
+@FeignClient(name="solicitacao-cartao", url="${cartoes.host}")
 public interface CartaoClientFeign {
 
     @GetMapping("/api/cartoes?idProposta")
--- proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/novaProposta/AnaliseSolicitacaoClient.java
+++ proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/novaProposta/AnaliseSolicitacaoClient.java
@@ -3,7 +3,7 @@ package br.com.zupacademy.gabrielaviana.proposta.novaProposta;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name="analise-solicitacao", url= "http://localhost:9999")
+@FeignClient(name="analise-solicitacao", url="${analise.host}")
 public interface AnaliseSolicitacaoClient {
 
     @PostMapping("/api/solicitacao")
