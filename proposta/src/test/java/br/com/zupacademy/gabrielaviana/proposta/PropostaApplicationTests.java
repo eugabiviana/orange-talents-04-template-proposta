e85df51c19b931b09f5266885d38078e6d80075c
eugabiviana <gabi.viana88@gmail.com>
2021-05-24 12:14:15 -0300

    Modificacoes no dockerfile

--- proposta/src/test/java/br/com/zupacademy/gabrielaviana/proposta/PropostaApplicationTests.java
+++ proposta/src/test/java/br/com/zupacademy/gabrielaviana/proposta/PropostaApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.zupacademy.gabrielaviana.proposta;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PropostaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.zupacademy.gabrielaviana.proposta;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PropostaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
