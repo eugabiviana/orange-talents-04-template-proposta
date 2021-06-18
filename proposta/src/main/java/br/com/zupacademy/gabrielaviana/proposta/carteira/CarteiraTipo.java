9e57ddb452ffccd59a40e376b410ab81c6129eab
eugabiviana <gabi.viana88@gmail.com>
2021-06-18 19:47:21 -0300

    Associacao com cartão Samsung Pay

--- proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/carteira/CarteiraTipo.java
+++ proposta/src/main/java/br/com/zupacademy/gabrielaviana/proposta/carteira/CarteiraTipo.java
@@ -1,6 +1,9 @@
 package br.com.zupacademy.gabrielaviana.proposta.carteira;
 
 public enum CarteiraTipo {
+    //implementando pagamento do tipo Paypal:
     PAYPAL,
+
+    //implementando pagamento do tipo Samsung:
     SAMSUNG_PAY;
 }
